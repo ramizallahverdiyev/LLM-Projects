ca415435ef5bffc690da5e608f6c569232745ca3
ramizallahverdiyev <ramiz.code0@gmail.com>
2025-12-29 12:03:35 +0400

    utils added

---
+++ 8_azerbaijani_ner_lora/src/utils/helpers.py
@@ -0,0 +1,14 @@
+import os
+import random
+import numpy as np
+import torch
+
+def set_seed(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+def make_dirs(path: str):
+    os.makedirs(path, exist_ok=True)
---
+++ 8_azerbaijani_ner_lora/src/utils/logging.py
@@ -0,0 +1,15 @@
+import logging
+
+def get_logger(name="NER_LoRA"):
+    """
+    Return a standard Python logger
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+    return logger
